721156fe0c5c95301e515c0b6550fa41e1e33e6a
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-11 16:42:31 -0500

    Solution for tree.rb

--- oop/trees.rb
+++ oop/trees.rb
@@ -1,9 +1,31 @@
 # Create a Tree class with a rings attribute and getter method.
 # Trees create a ring for every winter that passes
-# It should have a bear_fruit? method which should return true if the tree has fruit that year. the tree produces fruit when it has more than 7 rings but less than 15, but false otherwise.
+# It should have a bear_fruit? method which should return true if the tree has fruit that year. the tree produces
+# fruit when it has more than 7 rings but less than 15, but false otherwise.
 # The class should also have an winter_season method that increases the rings attribute by 1.
-# Hint: remember that with object oriented programming the object holds values in the attributes to describe it's current state. Even when we aren't reading it's attributes, they should be up to date.
+# Hint: remember that with object oriented programming the object holds values in the attributes to
+# describe it's current state. Even when we aren't reading it's attributes, they should be up to date.
 
+class Tree
+  attr_reader :rings
+
+  def initialize
+    @rings = 0
+  end
+
+  def bear_fruit?
+    if @rings > 7 and @rings < 15
+      return true
+    else
+      return false
+    end
+  end
+
+  def winter_season
+    @rings = @rings + 1
+  end
+
+end
 
 # Driver code - don't touch anything below this line.
 puts "TESTING the Tree class..."
